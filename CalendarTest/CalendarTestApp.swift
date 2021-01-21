ff7417a284858279588b47f48c84ad9fd7f31c4a
Larry Burris <lburris@mac.com>
2021-01-21 13:55:42 -0500

    Initial Commit

---
+++ CalendarTest/CalendarTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalendarTestApp.swift
+//  CalendarTest
+//
+//  Created by Larry Burris on 1/21/21.
+//
+
+import SwiftUI
+
+@main
+struct CalendarTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
