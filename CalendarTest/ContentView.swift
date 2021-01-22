6bfd726e3963d1103aa46c7c04cd1825d3d5ad9b
Larry Burris <lburris@mac.com>
2021-01-21 22:05:34 -0500

    Added DatePicker code

--- CalendarTest/ContentView.swift
+++ CalendarTest/ContentView.swift
@@ -4,18 +4,36 @@
 //
 //  Created by Larry Burris on 1/21/21.
 //
-
 import SwiftUI
 
-struct ContentView: View {
-    var body: some View {
-        Text("Hello, world!")
-            .padding()
+struct ContentView: View
+{
+    @State private var dateValue = Date()
+    
+    var body: some View
+    {
+        DatePicker("Choose a date:", selection: $dateValue, in: ...Date(), displayedComponents: .date)
+            .datePickerStyle(GraphicalDatePickerStyle())
+        
+        Text("Selected date is \(formatDate(dateValue))")
     }
 }
 
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
+struct ContentView_Previews: PreviewProvider
+{
+    static var previews: some View
+    {
         ContentView()
     }
 }
+
+func formatDate(_ date: Date) -> String
+{
+    let formatter = DateFormatter()
+    
+    formatter.dateFormat = "EEEE, MMM dd, yyyy"
+    
+    return formatter.string(from: date)
+}
+
+
